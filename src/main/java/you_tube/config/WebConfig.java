46c0744f752f06f8316a5080bf5a734b8174ecb0
OdilovR4 <odilovrahmatullo5822@gmail.com>
2024-11-21 18:36:53 +0500

    attach.http's added

---
+++ src/main/java/you_tube/config/WebConfig.java
@@ -0,0 +1,19 @@
+package you_tube.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public AppLanguageConverter appLanguageConverter() {
+        return new AppLanguageConverter();
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(appLanguageConverter());
+    }
+}
