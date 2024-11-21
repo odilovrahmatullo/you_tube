04145b92e2d5e6f48292ae41356c6b298b8dafae
OdilovR4 <odilovrahmatullo5822@gmail.com>
2024-11-21 18:37:05 +0500

    exception's added

---
+++ src/main/java/you_tube/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package you_tube.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
