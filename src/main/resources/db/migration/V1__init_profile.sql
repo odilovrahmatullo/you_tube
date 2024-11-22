972d3e84beea393d29dda31d9132158397d874f5
OdilovR4 <odilovrahmatullo5822@gmail.com>
2024-11-22 21:30:07 +0500

    internationalization's been updated

--- src/main/resources/db/migration/V1__init_profile.sql
+++
@@ -1,5 +0,0 @@
-insert into profile(name,surname,email,password,status,role, visible,created_date)
-values ('Admin','Adminov','admin@gmail.com','25d55ad283aa400af464c76d713c07ad','ACTIVE','ROLE_ADMIN',true,now()),
-       ('Moderator','Moderatorov','moderator@gmail.com','25d55ad283aa400af464c76d713c07ad', 'ACTIVE', 'ROLE_MODERATOR', true,now()),
-       ('Publisher','Publisherov','publisher@gmail.com','25d55ad283aa400af464c76d713c07ad', 'ACTIVE', 'ROLE_PUBLISHER', true,now());;
-
