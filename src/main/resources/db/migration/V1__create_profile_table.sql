fc7694914447a39bd0d1c0ed5a6f602cbf15ca15
ulugbek028 <ulugbeksamadov007@gmail.com>
2024-11-22 22:15:55 +0500

    Ulug'bek success

--- src/main/resources/db/migration/V1__create_profile_table.sql
+++ src/main/resources/db/migration/V1__create_profile_table.sql
@@ -1,12 +1,12 @@
--- CREATE TABLE profile (
---                          id SERIAL PRIMARY KEY,
---                          name VARCHAR(255),
---                          surname VARCHAR(255),
---                          email VARCHAR(255) UNIQUE,
---                          password VARCHAR(255),
---                          photo VARCHAR(255),
---                          role VARCHAR(255) CHECK (role IN ('ROLE_ADMIN', 'ROLE_MODERATOR', 'ROLE_PUBLISHER', 'ROLE_USER')),
---                          status VARCHAR(255),
---                          visible BOOLEAN,
---                          created_date TIMESTAMP
--- );
+CREATE TABLE profile (
+                         id SERIAL PRIMARY KEY,
+                         name VARCHAR(255),
+                         surname VARCHAR(255),
+                         email VARCHAR(255) UNIQUE,
+                         password VARCHAR(255),
+                         photo VARCHAR(255),
+                         role VARCHAR(255) CHECK (role IN ('ROLE_ADMIN', 'ROLE_MODERATOR', 'ROLE_PUBLISHER', 'ROLE_USER')),
+                         status VARCHAR(255),
+                         visible BOOLEAN,
+                         created_date TIMESTAMP
+);
