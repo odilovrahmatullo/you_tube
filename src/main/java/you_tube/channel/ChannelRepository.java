90c45646ca06fe435288c43b0536bfd44284d0f2
OdilovR4 <odilovrahmatullo5822@gmail.com>
2024-11-23 19:24:45 +0500

    added owner to sql file

--- src/main/java/you_tube/channel/ChannelRepository.java
+++ src/main/java/you_tube/channel/ChannelRepository.java
@@ -1,6 +1,26 @@
 package you_tube.channel;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ChannelRepository extends CrudRepository<ChannelEntity,Integer> {
+
+    @Modifying
+    @Transactional
+    @Query("UPdate ChannelEntity set photoId = ?2 where id = ?1")
+    int updatePhoto(String id, String photoId);
+
+    @Modifying
+    @Transactional
+    @Query("UPdate ChannelEntity set bannerId = ?2 where id = ?1")
+    int updateBanner(String id, String bannerId);
+
+    @Modifying
+    @Transactional
+    @Query("Update ChannelEntity set name = ?2,description = ?3 where id = ?1")
+    int updateInfo(String id,String name, String description);
 }
--- src/main/java/you_tube/profile/enums/ProfileRole.java
+++ src/main/java/you_tube/profile/enums/ProfileRole.java
@@ -1,5 +1,5 @@
 package you_tube.profile.enums;
 
 public enum ProfileRole {
-    ROLE_USER,ROLE_ADMIN,ROLE_MODERATOR,ROLE_PUBLISHER
+    ROLE_USER,ROLE_ADMIN,ROLE_MODERATOR,ROLE_PUBLISHER,ROLE_OWNER
 }
