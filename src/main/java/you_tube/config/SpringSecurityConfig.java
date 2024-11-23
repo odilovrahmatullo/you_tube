f8292396196f63fc0b4a8d7607e12f2b39205883
OdilovR4 <odilovrahmatullo5822@gmail.com>
2024-11-23 09:57:11 +0500

    SpringSecurityConfig's been added

---
+++ src/main/java/you_tube/config/SpringSecurityConfig.java
@@ -0,0 +1,58 @@
+package you_tube.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.UUID;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        // authentication - Foydalanuvchining identifikatsiya qilish.
+        // Ya'ni berilgan login va parolli user bor yoki yo'qligini aniqlash.
+        String password = UUID.randomUUID().toString();
+        System.out.println("User Password Mazgi: " + password);
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}" + password)
+                .roles("USER")
+                .build();
+
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(new InMemoryUserDetailsManager(user));
+        return authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // authorization - Foydalanuvchining tizimdagi huquqlarini tekshirish.
+        // Ya'ni foydalanuvchi murojat qilayotgan API-larni ishlatishga ruxsati bor yoki yo'qligini tekshirishdir.
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+            authorizationManagerRequestMatcherRegistry
+                    .anyRequest()
+                    .authenticated();
+        });
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(AbstractHttpConfigurer :: disable); // csrf yoqilgan
+        http.cors(AbstractHttpConfigurer :: disable); // cors yoqilgan
+
+        return http.build();
+    }
+
+
+}
