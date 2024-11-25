86ba8fe427ff7f23ea439fb033d6020da3cb3bb1
OdilovR4 <odilovrahmatullo5822@gmail.com>
2024-11-25 18:37:35 +0500

    Creating Video by Admin added

--- src/main/java/you_tube/video/VideoController.java
+++ src/main/java/you_tube/video/VideoController.java
@@ -1,6 +1,10 @@
 package you_tube.video;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,9 @@ public class VideoController {
     @Autowired
     private VideoService videoService;
 
+    @PostMapping
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> create(@RequestBody VideoDTO dto){
+        return ResponseEntity.ok(videoService.create(dto));
+    }
 }
--- src/main/java/you_tube/video/VideoRepository.java
+++ src/main/java/you_tube/video/VideoRepository.java
@@ -1,6 +1,9 @@
 package you_tube.video;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VideoRepository extends CrudRepository<VideoEntity,String> {
+    @Query("select count(v) > 0 from VideoEntity v where v.id = ?1 and v.categoryId = ?2 and v.visible = true")
+    Boolean existsByVideoIdAndCategoryId(String id, Integer categoryId);
 }
