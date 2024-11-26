e3a8a7f04a8f82d184753d932ab240a16adcca52
ulugbek028 <ulugbeksamadov007@gmail.com>
2024-11-26 19:08:56 +0500

    Ulug'bek playlist_video

--- src/main/java/you_tube/Playlist_video/repository/PlaylistVideoRepository.java
+++ src/main/java/you_tube/Playlist_video/repository/PlaylistVideoRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import you_tube.Playlist_video.entity.PlaylistVideoEntity;
 import you_tube.category.entity.CategoryEntity;
+import you_tube.video.VideoEntity;
+
+import java.util.List;
 
 @Repository
 public interface PlaylistVideoRepository extends CrudRepository<PlaylistVideoEntity,Integer> {
@@ -21,5 +24,7 @@ public interface PlaylistVideoRepository extends CrudRepository<PlaylistVideoEnt
     @Query("UPDATE PlaylistVideoEntity p SET p.visible = false WHERE p.playlistId = ?1 and p.videoId = ?2")
     int deletedVideoByPlaylis(Integer playlisId, String videoId);
 
+    @Query("select a.video from PlaylistVideoEntity a where a.playlistId = ?1 and a.visible = true")
+    List<VideoEntity> findAllVideosByPlaylistId(Integer playlistId);
 
 }
