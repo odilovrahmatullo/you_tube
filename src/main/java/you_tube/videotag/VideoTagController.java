66d2b397e7825b734e33b129420d44bee1cf6f2b
OdilovR4 <odilovrahmatullo5822@gmail.com>
2024-11-27 21:29:48 +0500

    creating video tag added

--- src/main/java/you_tube/videotag/VideoTagController.java
+++ src/main/java/you_tube/videotag/VideoTagController.java
@@ -1,6 +1,10 @@
 package you_tube.videotag;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +14,10 @@ public class VideoTagController {
     @Autowired
     private VideoTagService videoTagService;
 
+    @PostMapping
+    @PreAuthorize("hasRole('USER') or hasRole('OWNER')")
+    public ResponseEntity<?> create (@RequestBody VideoTagDTO dto){
+        return ResponseEntity.ok(videoTagService.create(dto));
+    }
 
 }
--- src/main/java/you_tube/videotag/VideoTagService.java
+++ src/main/java/you_tube/videotag/VideoTagService.java
@@ -3,8 +3,21 @@ package you_tube.videotag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class VideoTagService {
     @Autowired
     private VideoTagRepository videoTagRepository;
+
+    public VideoTagDTO create(VideoTagDTO dto) {
+        for (Integer i : dto.getTagIds()) {
+            VideoTagEntity entity = new VideoTagEntity();
+            entity.setVideoId(dto.getVideoId());
+            entity.setTagId(i);
+            entity.setCreatedDate(LocalDateTime.now());
+            videoTagRepository.save(entity);
+        }
+        return dto;
+    }
 }
