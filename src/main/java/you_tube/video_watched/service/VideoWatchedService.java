7934ac71ab16c23bdb1442ca07cabadee294e42a
ZaxriddinoV <78430544+ZaxriddinoV@users.noreply.github.com>
2024-11-28 13:07:08 +0500

    Video Watched create success

--- src/main/java/you_tube/video_watched/service/VideoWatchedService.java
+++ src/main/java/you_tube/video_watched/service/VideoWatchedService.java
@@ -2,15 +2,25 @@ package you_tube.video_watched.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import you_tube.exceptionHandler.AppBadException;
+import you_tube.profile.dto.ProfileDTO;
+import you_tube.profile.service.ProfileService;
+import you_tube.video.*;
 import you_tube.video_watched.entity.VideoWatchedEntity;
 import you_tube.video_watched.repository.VideoWatchedRepository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class VideoWatchedService {
     @Autowired
     private VideoWatchedRepository videoWatchedRepository;
+    @Autowired
+    private ProfileService profileService;
+    @Autowired
+    private VideoService videoService;
 
     public void createVideoWatched(Integer profileId,String videoId){
         VideoWatchedEntity videoWatchedEntity = new VideoWatchedEntity();
@@ -20,7 +30,17 @@ public class VideoWatchedService {
         videoWatchedRepository.save(videoWatchedEntity);
     }
 
-//    public VideoWatchedEntity getProfileId(Integer id){
-//        videoWatchedRepository.fi
-//    }
+    public List<VideoShortInfoDTO> getProfileId(Integer id){
+        profileService.getById(id);
+        List<VideoEntity> byProfileId = videoWatchedRepository.findByProfile_id(id);
+        List<VideoShortInfoDTO> videoDTOList = new ArrayList<>();
+        if(byProfileId.isEmpty()){
+            throw new AppBadException("No videos found");
+        }
+        for (VideoEntity videoEntity : byProfileId) {
+            VideoShortInfoDTO videoShortInfoDTO = videoService.mapperToInfo(videoEntity);
+            videoDTOList.add(videoShortInfoDTO);
+        }
+        return videoDTOList;
+    }
 }
