04ee3c5a233011464246aaa4bfcd1175cf49a9f5
OdilovR4 <odilovrahmatullo5822@gmail.com>
2024-11-29 17:15:51 +0500

    getting full info of video added except only LIKE

---
+++ src/main/java/you_tube/category/dto/SimpleCategoryDTO.java
@@ -0,0 +1,11 @@
+package you_tube.category.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SimpleCategoryDTO {
+    private Integer id;
+    private String name;
+}
